a772e47bfc4d233dc5f71cd8240a5a3188934790
Eneko96 <imr.aem2526@gmail.com>
2023-01-15 00:28:59 +0000

    getMy

--- src/rate/rate.service.ts
+++ src/rate/rate.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
-import { Repository } from 'typeorm';
 import { CreateRateDto } from './dto/create-rate.dto';
 import { Rate } from './rate.entity';
 import { RateRepository } from './rate.repository';
@@ -13,11 +12,17 @@ export class RateService {
     private ratesRepository: RateRepository,
   ) {}
 
+  private logger = new Logger('RateService');
+
   async getRates(): Promise<Rate[]> {
     return this.ratesRepository.find();
   }
 
   async createRate(rate: CreateRateDto, user: User): Promise<Rate> {
     return this.ratesRepository.save(rate, user);
+  }
+
+  async getMyRates(user: User): Promise<Rate[]> {
+    return this.ratesRepository.find({ where: { user } });
   }
 }
