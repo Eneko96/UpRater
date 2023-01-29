7b80c4cd0b8c08c6f414c33e21f71a69573092d7
Eneko96 <imr.aem2526@gmail.com>
2023-01-29 23:04:31 +0000

    yarn lock pre-commit

--- src/auth/get-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from './user.model';
-
-export const GetUser = createParamDecorator(
-  (_data, ctx: ExecutionContext): User => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.user;
-  },
-);
--- src/comment/dto/create-comment.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class CreateRateDto {
-  @IsNotEmpty()
-  readonly content: string;
-}
