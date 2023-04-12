d006d0881ca97f2c250d5e5979089cb292dc5c55
Eneko96 <imr.aem2526@gmail.com>
2023-04-13 00:25:43 +0100

    [FEAT] - relocate husky and remove pipe

--- src/lib/pipe.ts
+++
@@ -1,7 +0,0 @@
-export function pipe<T>(arr: T, pipe): T {
-  return pipe.reduce((result, stage) => {
-    return result[stage.action](
-      typeof stage.args === 'object' ? { ...stage.args } : stage.args,
-    );
-  }, arr);
-}
