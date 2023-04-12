55bcae2b097030c26cc264ac18b27211abb4d179
Eneko96 <imr.aem2526@gmail.com>
2023-04-12 13:52:57 +0100

    move docker to root

---
+++ dbStart-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# as we want to create and set our replica set, we need to wait for the mongo container to be up and running
+# so we wait for 5 seconds
+
+# then we execute the rs-init.sh script which will create the replica set and set the primary
+
+docker-compose up -d
+
+sleep 7
+
+docker exec mongo1 /scripts/rs-init.sh
