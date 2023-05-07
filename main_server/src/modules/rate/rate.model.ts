136c5a3cfee80e3250bc2a6127acbfe43b966286
Eneko96 <imr.aem2526@gmail.com>
2023-05-07 22:16:17 +0100

    fix

--- main_server/src/modules/rate/rate.model.ts
+++ main_server/src/modules/rate/rate.model.ts
@@ -8,24 +8,30 @@ export class Rate {
   _id: Types.ObjectId;
   @Prop()
   value: number;
+
   @Prop({ ref: 'Profile', type: Types.ObjectId })
   user_from: Types.ObjectId;
+
   @Prop({ ref: 'Profile', type: Types.ObjectId })
   user_to: Types.ObjectId;
+
   @Prop({ required: false })
   created_at: Date;
+
   @Prop()
   anon: boolean;
+
   @Prop()
   comment: string;
+
   @Prop()
   reactions_count: number;
+
   @Prop()
   comments_count: number;
+
   @Prop({ isFinite: true, min: 0, max: 1, default: 1 })
   weight: number;
-  // @Prop()
-  // how_close: number;
 }
 
 export const RateSchema = SchemaFactory.createForClass(Rate);
