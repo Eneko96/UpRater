8c82088f1cd7ee91121272efdbc3ea5c3473c0df
Eneko96 <imr.aem2526@gmail.com>
2023-05-21 21:16:07 +0100

    [FEAT] - migration

---
+++ main_server/__tests__/migration.sh
@@ -0,0 +1,5 @@
+mongoimport --host=localhost --port=27017 --db=mydatabase --collection=mycollection --file=data.json 
+
+mongoimport --host localhost --port 27017 --db test --collection users --file migrate_data_users.json --jsonArray &&
+mongoimport --host localhost --port 27017 --db test --collection rates --file migrate_data_rates.json --jsonArray &&
+mongoimport --host localhost --port 27017 --db test --collection profiles --file migrate_data_profiles.json --jsonArray
