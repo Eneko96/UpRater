c1524e46470eb2eebd2276e665c39eb7f99d0300
Eneko96 <imr.aem2526@gmail.com>
2023-05-22 16:59:46 +0100

    [FEAT] - fix directory

--- main_server/__tests__/migration.sh
+++ main_server/__tests__/migration.sh
@@ -1,3 +1,3 @@
-mongoimport --host localhost --port 27017 --db test --collection users --file migrate_data_users.json --jsonArray &&
-mongoimport --host localhost --port 27017 --db test --collection rates --file migrate_data_rates.json --jsonArray &&
-mongoimport --host localhost --port 27017 --db test --collection profiles --file migrate_data_profiles.json --jsonArray
+mongoimport --host localhost --port 27017 --db test --collection users --file /__tests__/migrate_data_users.json --jsonArray &&
+mongoimport --host localhost --port 27017 --db test --collection rates --file /__tests__/migrate_data_rates.json --jsonArray &&
+mongoimport --host localhost --port 27017 --db test --collection profiles --file /__tests__/migrate_data_profiles.json --jsonArray
