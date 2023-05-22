9c16f4c7b1fe3b5a173104fe6af08d5465ebcecf
Eneko96 <imr.aem2526@gmail.com>
2023-05-22 17:22:44 +0100

    [FEAT] - test without filder

--- main_server/__tests__/migration.sh
+++ main_server/__tests__/migration.sh
@@ -1,3 +1,3 @@
 mongoimport --host localhost --port 27017 --db test --collection users --file /__tests__/users.json --jsonArray
 mongoimport --host localhost --port 27017 --db test --collection rates --file /__tests__/rates.json --jsonArray
-mongoimport --host localhost --port 27017 --db test --collection profiles --file /__tests__/profiles.json --jsonArray
+mongoimport --host localhost --port 27017 --db test --collection profiles --file profiles.json --jsonArray
