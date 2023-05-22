91e6d755e15638c7652ba0e30536556ade5c96f6
Eneko96 <imr.aem2526@gmail.com>
2023-05-22 17:20:27 +0100

    [FEAT] - add jsons

--- main_server/__tests__/migration.sh
+++ main_server/__tests__/migration.sh
@@ -1,5 +1,3 @@
-echo "script_dir: $script_dir"
-echo "ls: $(ls)"
-mongoimport --host localhost --port 27017 --db test --collection users --file /__tests__/migrate_data_users.json --jsonArray
-mongoimport --host localhost --port 27017 --db test --collection rates --file /__tests__/migrate_data_rates.json --jsonArray
-mongoimport --host localhost --port 27017 --db test --collection profiles --file /__tests__/migrate_data_profiles.json --jsonArray
+mongoimport --host localhost --port 27017 --db test --collection users --file /__tests__/users.json --jsonArray
+mongoimport --host localhost --port 27017 --db test --collection rates --file /__tests__/rates.json --jsonArray
+mongoimport --host localhost --port 27017 --db test --collection profiles --file /__tests__/profiles.json --jsonArray
