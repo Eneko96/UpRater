b6593316bceccfd1315b0e605b607abd829554c8
Eneko96 <imr.aem2526@gmail.com>
2023-05-25 17:56:21 +0100

    [FEAT] - add test ls on pipeline

--- main_server/__tests__/migration.sh
+++ main_server/__tests__/migration.sh
@@ -1,3 +1,4 @@
+echo "ls $(ls)"
 mongoimport --host localhost --port 27017 --db test --collection users --file /__tests__/users.json --jsonArray
 mongoimport --host localhost --port 27017 --db test --collection rates --file /__tests__/rates.json --jsonArray
 mongoimport --host localhost --port 27017 --db test --collection profiles --file profiles.json --jsonArray
