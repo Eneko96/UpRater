cfb28e7dfc78b6317c65684d1d5ff2985c082774
Eneko96 <imr.aem2526@gmail.com>
2023-05-25 18:13:00 +0100

    [FEAT] - add cat script

--- main_server/__tests__/migration.sh
+++ main_server/__tests__/migration.sh
@@ -1,8 +1,4 @@
-echo "ls $(ls)"
-cat /__tests__/users.json
-cat users.json
-cd __tests__
-echo "ls x2 $(ls)"
-mongoimport --host localhost --port 27017 --db test --collection users --file /__tests__/users.json --jsonArray
-mongoimport --host localhost --port 27017 --db test --collection rates --file /__tests__/rates.json --jsonArray
+cd __tests__ 
+mongoimport --host localhost --port 27017 --db test --collection users --file users.json --jsonArray
+mongoimport --host localhost --port 27017 --db test --collection rates --file rates.json --jsonArray
 mongoimport --host localhost --port 27017 --db test --collection profiles --file profiles.json --jsonArray
